23261c4ae61e065aba7047517b5504ca7dfc8274
Masculinn <hello@burakdev.com>
2026-09-10 03:02:49 +0200

    fix: prod equation set to production in auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,7 +6,7 @@ export default function auth(request: NextRequest): boolean {
   const cookieValue = request.cookies.get("x-editor-admin")?.value;
   const userAgent = request.headers.get("user-agent");
 
-  if (process.env.NODE_ENV !== "production") {
+  if (process.env.NODE_ENV === "production") {
     if (!userAgent || !cookieValue) return false;
     const expectedAgent = userAgent.replace(/\s+/g, "");
     return (
